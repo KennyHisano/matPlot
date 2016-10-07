f8a459f122a8d07da4788a1cb44385994c36f0e6
Kenny H <nobi3892@gmail.com>
2016-10-07 19:02:26 +0900

    subplot

--- subplot.py
+++ subplot.py
@@ -1,5 +1,36 @@
 import random
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import plot, draw, show
+from matplotlib import style
 
+draw()
+
+style.use('fivethirtyeight')
 
 fig = plt.figure()
+
+def create_plots():
+	xs = []
+	ys = []
+
+	for i in range(10):
+		x = i
+		y = random.randrange(10)
+
+		xs.append(x)
+		ys.append(y)
+	return xs, ys
+
+ax1 = fig.add_subplot(211)
+#2 * 1 * select for each
+ax2 = fig.add_subplot(212)
+
+
+x,y = create_plots()
+ax1.plot(x,y)
+
+x,y = create_plots()
+ax2.plot(x,y)
+
+
+plt.show()
